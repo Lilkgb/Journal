bf10dbb5ff24915799cb93134aa5a0a624fdb3ae
Philip Eskins and Ryan McLean <ryan@>
2019-03-11 11:52:07 -0700

    add vowel and const count, add teaser

--- src/index.js
+++ src/index.js
@@ -18,6 +18,36 @@ Journal.prototype.numWords = function(journal) {
   $(".journalBodyCount").append("There are " + numCount + " words in your body.")
 }
 
+Journal.prototype.numVowels = function(journal) {
+  var numCount = 0;
+  var otherCount = 0;
+  var vowels = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"];
+  var consts = ["b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "v", "x", "z"];
+  for (var i=0; i<journal.length; i++) {
+    for (var j=0; j<vowels.length; j++) {
+      if (journal[i] === vowels[j]) {
+        numCount++;
+      }
+    }
+    for (var c=0; c<consts.length; c++) {
+      if (journal[i] === consts[c]) {
+        otherCount++;
+      }
+    }
+  }
+  $(".journalVowels").append("There are " + numCount + " vowels in your body.");
+  $(".journalConsts").append("There are " + otherCount + " consonant in your body.");
+}
+
+Journal.prototype.getTeaser = function(journal) {
+  var words = journal.split(' ');
+  if (words.length > 8) {
+    var output = words.splice(0, 8);
+    var output = output.join(' ')
+  }
+  $(".journalTeaser").append("Teaser: " + output);
+}
+
 $(document).ready(function() {
   $("#journalEntry").submit(function(event){
     event.preventDefault();
@@ -25,6 +55,8 @@ $(document).ready(function() {
     var journalBody = $("#journalBody").val();
     var journalStore = new Journal(journalTitle, journalBody);
     journalStore.numWords(journalBody);
+    journalStore.numVowels(journalBody);
+    journalStore.getTeaser(journalBody);
     $(".journalTitleDisplay").append(journalTitle);
     $(".journalBodyDisplay").append(journalBody);
   });
